a187e66bf8b3120407edf588002478f130d6f9c8
mario <mario@192.168.0.157>
2018-09-16 00:18:29 +0200

    Add new java project

---
+++ java/src/java_main_project/JavaMain.java
@@ -0,0 +1,14 @@
+package java_main_project;
+
+public class JavaMain {
+
+	public JavaMain() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
